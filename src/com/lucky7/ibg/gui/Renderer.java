537f6c367c47bb1cbebd2686255596902216f603
Abel Acosta <abelacosta1596@gmail.com>
2019-06-07 12:59:03 -0700

    Menu now renders illuminati image

--- src/com/lucky7/ibg/gui/Renderer.java
+++ src/com/lucky7/ibg/gui/Renderer.java
@@ -2,7 +2,12 @@ package com.lucky7.ibg.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.lucky7.ibg.Game;
@@ -12,9 +17,54 @@ public class Renderer extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
 	Game game;
+	ScreenState state;
+	
+	// Components
+	JButton playButton;
+	JButton exitButton;
+	
+	// Images
+	BufferedImage illuminati_img;
 	
 	public Renderer(Game g) {
 		this.game = g;
+		this.state = ScreenState.MainMenu;
+		init();
+		addComponents();
+		loadResources();
+	}
+	
+	void renderGame(Graphics g) {
+		
+	}
+	
+	void renderMainMenu(Graphics g) {
+		g.drawImage(illuminati_img, 0, 0, super.getWidth(), super.getHeight(), this);
+	}
+	
+	void renderPlayMenu(Graphics g) {
+		
+	}
+	
+	void init() {
+		// Initializes objects
+		playButton = new JButton("Play");
+		exitButton = new JButton("Exit");
+	}
+	
+	void addComponents() {
+		// Adds buttons and other components to panel
+		add(playButton);
+		add(exitButton);
+	}
+	
+	void loadResources() {
+		// loads images and other resources
+		try {
+			illuminati_img = ImageIO.read(new File("res/illuminati.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -24,5 +74,19 @@ public class Renderer extends JPanel{
 		// Black Background
 		g.setColor(Color.black);
 		g.fillRect(0, 0, super.getWidth(), super.getHeight());
+		
+		switch(state){
+		case Game:
+			renderGame(g);
+			break;
+		case MainMenu:
+			renderMainMenu(g);
+			break;
+		case PlayMenu:
+			renderPlayMenu(g);
+			break;
+		default:
+			break;
+		}
 	}
 }
