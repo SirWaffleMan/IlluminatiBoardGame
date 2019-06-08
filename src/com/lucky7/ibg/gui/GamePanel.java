4e888cea11807e08af3cbbb08cfc690c180cc6ba
Abel Acosta <abelacosta1596@gmail.com>
2019-06-07 19:07:20 -0700

    GamePanel renders black screen by default

--- src/com/lucky7/ibg/gui/GamePanel.java
+++ src/com/lucky7/ibg/gui/GamePanel.java
@@ -1,9 +1,19 @@
 package com.lucky7.ibg.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, super.getWidth(), super.getHeight());
+	}
 }
