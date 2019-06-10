1153a1d9a6522345f55b5ab0705b2ab5d3ec7cbe
Abel Acosta <abelacosta1596@gmail.com>
2019-06-10 15:46:31 -0700

    Created all Illuminati Cards

---
+++ src/com/lucky7/ibg/card/illuminati/TheBavarianIlluminati.java
@@ -0,0 +1,9 @@
+package com.lucky7.ibg.card.illuminati;
+
+public class TheBavarianIlluminati extends IlluminatiCard{
+
+	public TheBavarianIlluminati() {
+		super("The Bavarian Illuminati", 10, 10, 9);
+	}
+
+}
