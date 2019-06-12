4026254339d531d9b0c6146d6dcbbbfdffb56f62
saul-h <saulhernandez217@gmail.com>
2019-06-12 16:41:08 -0700

    Created ChatBox

---
+++ src/com/lucky7/ibg/gui/ChatBox.java
@@ -0,0 +1,17 @@
+package com.lucky7.ibg.gui;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ChatBox extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	JTextField messageField;
+	JButton sendButton;
+	
+	public ChatBox() {
+		messageField = new JTextField(10);
+		sendButton = new JButton("Send");
+	}
+}
