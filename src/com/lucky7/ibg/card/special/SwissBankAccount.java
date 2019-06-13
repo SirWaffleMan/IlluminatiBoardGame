05f0c2a72d665a51ef6e969e2bc9ac094bfb7218
Doge <anemailaccount26@yahoo.com>
2019-06-13 14:17:05 -0700

    Special Cards Done

---
+++ src/com/lucky7/ibg/card/special/SwissBankAccount.java
@@ -0,0 +1,10 @@
+package com.lucky7.ibg.card.special;
+
+public class SwissBankAccount extends SpecialCard{
+
+	public SwissBankAccount() {
+		super(	"Swiss Bank Account", "Exchange this card, at any time, for"
+				+ "25 MB to be placed in your illuminati treasury.");
+	}
+
+}
---
+++ src/com/lucky7/ibg/card/special/TimeWarp.java
@@ -0,0 +1,12 @@
+package com.lucky7.ibg.card.special;
+
+public class TimeWarp extends SpecialCard{
+
+	public TimeWarp() {
+		super(	"Time Warp", "Play this card during your turn to "
+				+ "allow you one extra action on that turn. /n"
+				+ "Remember, no individual group except the UFOs may"
+				+ "act more than once during the turn.");
+	}
+
+}
