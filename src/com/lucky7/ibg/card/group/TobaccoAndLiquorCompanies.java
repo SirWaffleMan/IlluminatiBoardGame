f99ca055ce1f5baea930d752ff189896f2af1119
saul-h <saulhernandez217@gmail.com>
2019-06-25 16:34:10 -0700

    added tobacco and liquor

---
+++ src/com/lucky7/ibg/card/group/TobaccoAndLiquorCompanies.java
@@ -0,0 +1,11 @@
+package com.lucky7.ibg.card.group;
+
+import com.lucky7.ibg.card.Alignments;
+
+public class TobaccoAndLiquorCompanies extends GroupCard{
+	public TobaccoAndLiquorCompanies() {
+		super("Tobacco & Liquor Companies", 4, -1, 3, 3);
+		addAlignment(Alignments.Straight);
+
+	}
+}
