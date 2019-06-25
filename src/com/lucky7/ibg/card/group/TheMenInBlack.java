d3a75bd8b2ba1f80512cab6d93a9c9f3242c0c25
saul-h <saulhernandez217@gmail.com>
2019-06-25 16:40:45 -0700

    men in black and united nations

---
+++ src/com/lucky7/ibg/card/group/TheMenInBlack.java
@@ -0,0 +1,12 @@
+package com.lucky7.ibg.card.group;
+
+import com.lucky7.ibg.card.Alignments;
+
+public class TheMenInBlack extends GroupCard{
+	public TheMenInBlack() {
+		super("The Men In Black", 0, 2, 6, 1);
+		addAlignment(Alignments.Weird); 
+		addAlignment(Alignments.Criminal); 
+
+	}
+}
