3c4bba8005744a00d9435128d56cfb2aae1eb154
saul-h <saulhernandez217@gmail.com>
2019-06-25 16:05:25 -0700

    VideoGames

---
+++ src/com/lucky7/ibg/card/group/VideoGames.java
@@ -0,0 +1,10 @@
+package com.lucky7.ibg.card.group;
+
+import com.lucky7.ibg.card.Alignments;
+
+public class VideoGames extends GroupCard{
+	public VideoGames() {
+		super("VideoGames", 2, -1, 3, 7);
+		addAlignment(Alignments.Media);
+	}
+}
