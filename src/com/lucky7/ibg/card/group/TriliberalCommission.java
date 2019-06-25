c93a964be4acfcb6445052aa66b24c9fe315f480
saul-h <saulhernandez217@gmail.com>
2019-06-25 16:30:56 -0700

    TriliberalCommission

---
+++ src/com/lucky7/ibg/card/group/TriliberalCommission.java
@@ -0,0 +1,12 @@
+package com.lucky7.ibg.card.group;
+
+import com.lucky7.ibg.card.Alignments;
+
+public class TriliberalCommission extends GroupCard{
+	public TriliberalCommission() {
+		super("Triliberal Commission", 5, -1, 6, 3);
+		addAlignment(Alignments.Straight); 
+		addAlignment(Alignments.Liberal); 
+
+	}
+}
