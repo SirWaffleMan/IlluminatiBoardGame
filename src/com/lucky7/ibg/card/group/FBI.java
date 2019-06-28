444fe71b4640cea36368c88c10b7ded973cdc9ef
saul-h <saulhernandez217@gmail.com>
2019-06-28 12:32:16 -0700

    set hasTopArrow and hasBottomArrow to true

--- src/com/lucky7/ibg/card/group/FBI.java
+++ src/com/lucky7/ibg/card/group/FBI.java
@@ -7,6 +7,9 @@ public class FBI extends GroupCard{
 	public FBI() {
 		super("FBI", 4, 2, 6, 0);
 		addAlignment(Alignments.Straight);
+		
+		hasTopArrow = true;
+		hasBottomArrow = true;
 	}
 
 }
--- src/com/lucky7/ibg/card/group/IRS.java
+++ src/com/lucky7/ibg/card/group/IRS.java
@@ -7,6 +7,9 @@ public class IRS extends GroupCard{
 	public IRS() {
 		super("IRS", 5, 3, 5, 0);
 		addAlignment(Alignments.Criminal);
+		
+		hasTopArrow = true;
+		hasBottomArrow = true;
 	}
 	
 }
